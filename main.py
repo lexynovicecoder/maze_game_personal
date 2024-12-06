afc4893373d5f416f2f09f4d247f6669a865bc40
lexynovicecoder <alexandradidieye21@gmail.com>
2024-12-06 11:28:15 +0000

    blitted ground image and set up window. Created utils file for some functions

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
 import pygame
+
+
+GROUND = pygame.image.load("dirt_floor.jpg")
+WIDTH, HEIGHT = GROUND.get_width(), GROUND.get_height()
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+
+run = True
+clock = pygame.time.Clock()
+
+while run:
+    clock.tick(60)
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            break
+
+    WIN.blit(GROUND,(0, 0))
+    pygame.display.update()
+
+pygame.quit()
