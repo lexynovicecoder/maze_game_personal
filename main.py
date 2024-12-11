ae3146891c3f653d0f9136c52e3c252ceb5dacfc
lexynovicecoder <alexandradidieye21@gmail.com>
2024-12-11 11:26:31 +0100

    created function in utils file to scale image

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import pygame
-
+from utils import *
 
 GROUND = pygame.image.load("dirt_floor.jpg")
 WIDTH, HEIGHT = GROUND.get_width(), GROUND.get_height()
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+
+MAZE = scale_image(pygame.image.load("mazes/stone_maze1.png"),1)
 
 run = True
 clock = pygame.time.Clock()
@@ -13,10 +15,12 @@ while run:
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            pygame.quit()
+            run = False
             break
 
     WIN.blit(GROUND,(0, 0))
+    WIN.blit(MAZE,(170, 100))
+
     pygame.display.update()
 
 pygame.quit()
---
+++ utils.py
@@ -0,0 +1,4 @@
+import pygame
+def scale_image(image,factor):
+    size = round(image.get_width()*factor), round(image.get_height()*factor)
+    return pygame.transform.scale(image, size)
