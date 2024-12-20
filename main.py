df67ae9c57029da4ea8c43e60071a07dd74420ba
lexynovicecoder <alexandradidieye21@gmail.com>
2024-12-20 10:37:25 +0000

    blitting idle animation on screen

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+import os
 from utils import *
 
 GROUND = pygame.image.load("dirt_floor.jpg")
@@ -6,13 +7,10 @@ WIDTH, HEIGHT = GROUND.get_width(), GROUND.get_height()
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 
 MAZE = scale_image(pygame.image.load("mazes/stone_maze1.png"),1.7)
-DEF_CHAR = scale_image(pygame.image.load("def_char/da_idle1.png"),0.5)
-
-IDLE_FRAMES = [scale_image(pygame.image.load(f"def_char/da_idle{i}.png"), 0.5) for i in range(1, 5)]
+DEF_CHAR = "def_char"
 
 
-run = True
-clock = pygame.time.Clock()
+
 
 class Sprites:
     def __init__(self,max_vel,acceleration):
@@ -23,23 +21,59 @@ class Sprites:
 
 
 
-    # def walk_down(self):
-    #     self.y += self.acceleration
 
 
 class Character(Sprites):
-    IMG = DEF_CHAR
+    IMG = scale_image(pygame.image.load(f"{DEF_CHAR}/da_idle1.png"),0.5)
     START_POS = (210, 200)
 
+    def __init__(self, max_vel, acceleration):
+        super().__init__(max_vel, acceleration)
+        self.image_folder = DEF_CHAR
+        self.frame_count = 5
+        self.frames = []
+        self.current_frame = 1
+        self.frame_delay = 100
+        self.last_update = pygame.time.get_ticks()
+        self.is_moving = False
+
+        for i in range(1, self.frame_count):
+            frame_path = os.path.join(self.image_folder, f"da_idle{i}.png")
+            frame = scale_image(pygame.image.load(frame_path),0.5)
+            self.frames.append(frame)
+
     def draw(self, win):
-        win.blit(self.IMG, (self.x, self.y))
+        current_img = self.frames[self.current_frame]
+
+        # Center the frame on the character's position
+        frame_rect = current_img.get_rect(center=(self.x, self.y))
+
+        # Blit the current frame
+        win.blit(current_img, frame_rect.topleft)
     def walk_down(self, moving):
         if moving:
+            self.is_moving = True
             if self.velocity_y <= self.max_vel:
                 self.velocity_y += self.acceleration
             else:
                 self.velocity_y = self.max_vel
             self.y += self.velocity_y
+        else:
+            self.is_moving = False
+
+    def update_animation(self):
+        if self.is_moving is False:
+            now = pygame.time.get_ticks()
+            if now - self.last_update > self.frame_delay:
+                self.current_frame = (self.current_frame + 1) % len(self.frames)  # Loop back to the first frame
+                self.last_update = now
+
+
+
+
+
+
+
 
 
 
@@ -47,6 +81,9 @@ class Character(Sprites):
 
 
 character = Character(6,0.5)
+
+run = True
+clock = pygame.time.Clock()
 
 while run:
     clock.tick(60)
@@ -57,10 +94,15 @@ while run:
             break
 
 
-    WIN.blit(GROUND,(0, 0))
-    WIN.blit(MAZE,(130, 130))
+
     keys = pygame.key.get_pressed()
     character.walk_down(keys[pygame.K_DOWN])
+
+
+    character.update_animation()
+    WIN.blit(GROUND, (0, 0))
+    WIN.blit(MAZE, (130, 130))
+
     character.draw(WIN)
 
 
